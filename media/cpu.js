607b99a36e3b94e6de1bed99d0dab20b7f7ff9bb
Fabian Vogler <fabian@equivalence.ch>
2011-11-12 16:19:27 +0100

    Load binary and display memory

--- media/cpu.js
+++ media/cpu.js
@@ -4,6 +4,9 @@ var INSTRUCTIONS = {
         bits: function (rnr) {
             return '0000' + rnr + '10 10000000';
         },
+        optcode: function (rnr) {
+            return 'CLR R' + rnr;
+        },
         execute: function (rnr) {
             this.register[rnr] = 0;
         }
@@ -13,11 +16,29 @@ var INSTRUCTIONS = {
         bits: function (rnr, adr) {
             return '0100' + rnr + adr;
         },
+        optcode: function (rnr, adr) {
+            return 'LWDD R' + rnr + ', #' + adr;
+        },
         execute: function (rnr, adr) {
             this.register[rnr] = bin2dec(this.memory[adr] + this.memory[adr + 1]);
         }
     }
 };
+
+function parseBin(memory, input, offset) {
+    
+    input = input.replace(/[^01]*/g, '');
+    
+    if (input.length % 16 != 0) {
+        return false; // invalid input
+    }
+    
+    for (var i = 0; i < (input.length / 8); i++) {
+        memory[i + offset] = input.slice(i*8, (i+1)*8);
+    }
+    
+    return memory;
+}
 
 function bin2dec(n) {
     n = parseInt(n, 2);
@@ -57,30 +78,7 @@ var CPU = function () {
     // main loop
     while (this.position < 104) {
         
-        var current = this.memory[this.position] + this.memory[this.position + 1];
-        
-        for (name in INSTRUCTIONS) {
-            
-            var instruction = INSTRUCTIONS[name];
-            
-            // check if regex matches
-            var match = current.match(instruction.regex);
-            if (match != null) {
-                
-                console.log(name);
-                
-                // convert to decimal
-                var args = match.slice(1);
-                for (i in args) {
-                    args[i] = bin2dec(args[i]);
-                }
-                
-                // execute instruction
-                instruction.execute.apply(this, args);
-                
-                break;
-            }
-        }
+        this.decode(this.position).execute();
         
         this.position = this.position + 2;
         
@@ -88,5 +86,89 @@ var CPU = function () {
     }
 };
 
-var cpu = new CPU();
+CPU.prototype.decode = function (position) {
+    
+    var bits = this.memory[position] + this.memory[position + 1];
+    
+    for (name in INSTRUCTIONS) {
+        
+        var instruction = INSTRUCTIONS[name];
+        
+        // check if regex matches
+        var match = bits.match(instruction.regex);
+        if (match != null) {
+            
+            console.log(name);
+            
+            // convert to decimal
+            var args = match.slice(1);
+            for (i in args) {
+                args[i] = bin2dec(args[i]);
+            }
+            
+            var that = this;
+            return {
+                execute: function () {
+                    instruction.execute.apply(that, args);
+                },
+                optcode: function () {
+                    return instruction.optcode.apply(that, args);
+                }
+            };
+        }
+    }
+    
+    return {
+        execute: function () {
+            // do nothing
+        },
+        optcode: function () {
+            return '';
+        }
+    }
+};
+
+CPU.prototype.load = function (empty, converter) {
+
+    var memory = [];
+    for (var i = 1; i < 530; i++) {
+        memory[i] = '00000000';
+    }
+
+    var instructions = prompt('Please paste instructions:', empty);
+    memory = converter(memory, instructions, 100);
+
+    if (instructions) {
+    
+        var input = prompt('Please paste input:', empty);
+        memory = converter(memory, input, 500);
+        
+        if (memory) {
+            
+            this.memory = memory;
+            this.display();
+            
+        } else {
+            alert('Invalid memory!');
+        }
+
+    } else {
+        alert('Invalid instructions!');
+    }
+};
+
+CPU.prototype.display = function () {
+
+    // instructions
+    $('.instructions .values').empty();
+    for (var i = 100; i < 500; i += 2) {
+        $('.instructions .values').append('<li><em>' + i + '</em> ' + this.memory[i] + ' ' + this.memory[i+1] + ' <em>' + this.decode(i).optcode() + '</em></li>');
+    }
+
+    // instructions
+    $('.memory .values').empty();
+    for (var i = 500; i < 530; i += 2) {
+        $('.memory .values').append('<li><em>' + i + '</em> ' + this.memory[i] + ' ' + this.memory[i+1] + ' <em>' + bin2dec(this.memory[i] + this.memory[i+1]) + '</em></li>');
+    }
+};
 
